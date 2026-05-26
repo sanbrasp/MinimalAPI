70c63dcea65f9ec181a398c4c061633801ccbcd7
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-05-26 20:09:00 +0200

    feat: add NetworkController.cs

---
+++ Controllers/NetworkController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MinimalAPI.Services;
+
+namespace MinimalAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NetworkController : ControllerBase
+{
+    private readonly NetworkService _networkService;
+    
+    public NetworkController(NetworkService networkService)
+    {
+        _networkService = networkService;
+    }
+
+
+    [HttpGet("connected")]
+    public IActionResult IsConnected([FromQuery] string from, [FromQuery] string to)
+    {
+        var result = _networkService.IsConnected(from, to);
+        return Ok(new { from, to, connected = result });
+    }
+}
