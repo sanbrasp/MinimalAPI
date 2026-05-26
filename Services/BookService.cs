89f2ecadfc16b49a9ab0ec62aed50b10801a99b2
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-05-26 20:08:48 +0200

    feat: add BookService.cs

---
+++ Services/BookService.cs
@@ -0,0 +1,15 @@
+﻿namespace MinimalAPI.Services;
+
+public class BookService
+{
+    private readonly List<Book> _books = new()
+    {
+        new Book(1, "The Hobbit", "J.R.R. Tolkien"),
+        new Book(2, "1984", "George Orwell"),
+        new Book(3, "Dune", "Frank Herbert")
+    };
+
+    public List<Book> GetAll() => _books;
+    
+    public Book? GetById(int id) => _books.FirstOrDefault(b => b.Id == id);
+}
