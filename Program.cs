0f4a178ea8fb6f8d2fbbac516cd3a7028e3f4e9d
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-05-26 20:09:19 +0200

    update: Program.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,16 @@
+using MinimalAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSingleton<BookService>();
+builder.Services.AddSingleton<NetworkService>();
+
 var app = builder.Build();
 
-// Data
-var books = new List<Book>
-{
-    new Book(1, "The Hobbit", "J.R.R. Tolkien"),
-    new Book(2, "1984", "George Orwell"),
-    new Book(3, "Dune", "Frank Herbert")
-};
-
-
-app.MapGet("/books", () => books);
-
-app.MapGet("/books/{id}", (int id) =>
-{
-    var book = books.FirstOrDefault(b => b.Id == id);
-    return book is not null ? Results.Ok(book) : Results.NotFound();
-});
-
+app.MapControllers();
 app.Run();
 
 
 // Model
-record Book(int Id, string Title, string Author);
+public record Book(int Id, string Title, string Author);
