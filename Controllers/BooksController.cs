ee06aa87a6208da156746f08acab3aa621e8c5a6
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-05-26 20:08:37 +0200

    feat: add BooksController.cs

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinimalAPI.Services;
+
+namespace MinimalAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly BookService _bookService;
+
+    public BooksController(BookService bookService)
+    {
+        _bookService = bookService;
+    }
+
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(_bookService.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var book = _bookService.GetById(id);
+        return book is not null ? Ok(book) : NotFound();
+    }
+
+    
+}
